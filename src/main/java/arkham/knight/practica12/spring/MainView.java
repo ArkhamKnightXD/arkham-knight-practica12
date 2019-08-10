f3d0c4a574975fd8c7c9bb3f3c8a65e5b4317218
arkhamknight <karvin_jimenez@hotmail.com>
2019-08-10 18:31:17 -0400

    cambios

--- src/main/java/arkham/knight/practica12/spring/MainView.java
+++ src/main/java/arkham/knight/practica12/spring/MainView.java
@@ -7,13 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
+//En mainview es recomendable siempre trabajar la pagina de inicio
 @Route
 public class MainView extends VerticalLayout {
 
     public MainView(@Autowired MessageBean bean) {
+
+
         Button button = new Button("Click me",
                 e -> Notification.show(bean.getMessage()));
         add(button);
+
+
     }
 
 }
